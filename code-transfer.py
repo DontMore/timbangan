3f6bb0bfdbcf6aeca8de6e763ab5189ffacd4c02
DontMore <54996848+DontMore@users.noreply.github.com>
2023-12-13 15:22:24 +0700

    Update code-transfer.py

--- code-transfer.py
+++ code-transfer.py
@@ -1,25 +1,50 @@
-# Mengimpor library pyserial
-import serial
+# Mengimpor library Tkinter
+import tkinter as tk
 
-# Membuat objek serial dengan parameter sesuai perangkat yang terhubung
-ser = serial.Serial(
-        port= '/dev/ttyUSB1',
-        baudrate='1200',
-        parity=serial.PARITY_ODD,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.SEVENBITS,
-        timeout=None
-    )
+# Membuat objek window sebagai root window
+window = tk.Tk()
 
-# Membuka port serial jika belum terbuka
-if not ser.is_open:
-    ser.open()
+# Memberi judul window
+window.title("Serial Communication")
 
-# Membaca data dari port serial secara terus menerus
-while True:
-    # Membaca sebanyak 100 byte dari port serial
+# Membuat frame untuk menampung widget
+frame = tk.Frame(window)
+
+# Membuat label untuk menampilkan data dari port serial
+label = tk.Label(frame, text="Data from serial port:")
+
+# Membuat text widget untuk menampilkan data dari port serial
+text = tk.Text(frame, height=10, width=40)
+
+# Membuat scrollbar untuk text widget
+scrollbar = tk.Scrollbar(frame, command=text.yview)
+
+# Menghubungkan scrollbar dengan text widget
+text.config(yscrollcommand=scrollbar.set)
+
+# Menempatkan widget pada frame dengan grid layout manager
+label.grid(row=0, column=0, sticky="w")
+text.grid(row=1, column=0, sticky="nsew")
+scrollbar.grid(row=1, column=1, sticky="ns")
+
+# Menyesuaikan ukuran frame sesuai dengan ukuran window
+frame.pack(fill=tk.BOTH, expand=True)
+
+# Membuat fungsi untuk membaca data dari port serial dan menampilkannya pada text widget
+def read_serial():
+    # Menggunakan kode yang Anda berikan untuk membaca data dari port serial
     data = ser.readline()
     dataPrint = data.decode('utf-8')
-    # Jika ada data yang diterima, cetak data tersebut
+    # Jika ada data yang diterima, tambahkan data tersebut ke text widget
     if dataPrint:
-        print(dataPrint)
+        text.insert(tk.END, dataPrint)
+        # Scroll text widget ke bawah agar data terbaru terlihat
+        text.see(tk.END)
+    # Memanggil fungsi ini lagi setelah 100 milidetik
+    window.after(100, read_serial)
+
+# Memanggil fungsi read_serial untuk pertama kali
+read_serial()
+
+# Menjalankan main loop dari window
+window.mainloop()
